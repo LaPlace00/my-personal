faa9499df49077d46fd978ca0986351ddd98ad11
LaPlace00 <85988297+LaPlace00@users.noreply.github.com>
2021-06-16 20:49:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,20 +5,8 @@ self.addEventListener("install", e => {
              "./",
              "./src/index.js",
              "./css/styles.css",
-             "./css/pink.css",
-             "./picture/csharp.png",
-             "./picture/hob1.jpg",
-             "./picture/hob2.jpeg",
-             "./picture/hob3.jpg",
-             "./picture/java.png",
-             "./picture/m1.jpg",
-             "./picture/m2.jpg",
-             "./picture/m3.jpg",
-             "./picture/me.jpg",
-             "./picture/mv1.jpg",
-             "./picture/mv2.jpg",
-             "./picture/mv3.jpg",
-             "./picture/pyt.png"
+             "./picture/192.png",
+             "./picture/512.png"
          ]);
       })
     );
